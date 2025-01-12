ce4f45dbc5e7d6209cd0e8b694c7e5abf29c6aaa
serkanok1983 <serkanok1983@yahoo.com>
2025-01-13 01:06:02 +0300

    3.6 Phonebook backend step 6

--- index.js
+++ index.js
@@ -53,6 +53,16 @@ app.delete("/api/persons/:id", (req, res) => {
 
 app.post("/api/persons", (req, res) => {
   const body = req.body;
+  if (!body.name || !body.number) {
+    return res.status(400).json({
+      error: "name or number is missing",
+    });
+  }
+  if (persons.find((person) => person.name === body.name)) {
+    return res.status(400).json({
+      error: "name must be unique",
+    });
+  }
   const person = {
     id: Math.floor(Math.random() * 1000),
     name: body.name,
