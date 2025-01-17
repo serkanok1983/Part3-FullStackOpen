a7acea72b91438aae13c16a430ac2a33b53a52a4
serkanok1983 <serkanok1983@yahoo.com>
2025-01-18 00:20:42 +0300

    3.9 Phonebook backend step 9, 3.10 Phonebook backend step 10, 3.11 Full Stack Phonebook

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
 
+app.use(express.static('dist'))
 app.use(express.json());
+app.use(cors());
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :req-body"));
 
 morgan.token("req-body", (req) => {
@@ -80,7 +83,7 @@ app.post("/api/persons", (req, res) => {
   res.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
